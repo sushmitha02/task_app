7e60dcf0e3a0cf23990cb2a33d743e5423efddc8
sushmitha02 <sushmithahgowda27@gmail.com>
2019-07-22 12:54:34 +0530

    Gave connection after sigin to land in task dashboard page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
-	stored_location_for(resource) || page_dashboard_path
+		stored_location_for(resource) || pages_dashboard_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :tasks
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   get 'pages/home'
   get 'pages/dashboard'
 
-  root to: 'pages#dashboard'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
